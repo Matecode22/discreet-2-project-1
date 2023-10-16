dc69d1dff050615997ea035429298350626877c8
Mateo Berrio <mateoberriov5@gmail.com>
2023-10-15 22:43:49 -0500

    engineering method

---
+++ src/model/Reminder.java
@@ -0,0 +1,24 @@
+package model;
+
+public class Reminder extends Event {
+    private String time;
+
+    public Reminder(String title, String description, String deadline, String time) {
+        super(title, description, deadline);
+        this.time = time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    @Override
+    public String toString() {
+        return "Reminder{" +
+                "title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", deadline='" + deadline + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,19 +1,112 @@
 package ui;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import model.Controller;
+
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    private Scanner scanner;
+    private Controller controller;
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    public Main() {
+        scanner = new Scanner(System.in);
+        controller = new Controller();
+    }
+
+    public static void main(String[] args){
+        Main m = new Main();
+        m.start();
+    }
+
+    private void start() {
+        int choice;
+
+        do {
+            System.out.println("-------- Task and Reminder Management System --------");
+            System.out.println("1. Add Task");
+            System.out.println("2. Add Reminder");
+            System.out.println("3. Show Events");
+            System.out.println("4. Remove Event");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+
+            choice = scanner.nextInt();
+            scanner.nextLine(); // Consume the newline character
+
+            switch (choice) {
+                case 1:
+                    addTask();
+                    break;
+                case 2:
+                    addReminder();
+                    break;
+                case 3:
+                    showEvents();
+                    break;
+                case 4:
+                    removeEvent();
+                    break;
+                case 5:
+                    System.out.println("Exiting the program. Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        } while (choice != 5);
+
+        scanner.close();
+    }
+
+    private void addTask() {
+        System.out.print("Enter task title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter task description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Enter task deadline: ");
+        String deadline = scanner.nextLine();
+
+        System.out.print("Enter task priority: ");
+        int priority = scanner.nextInt();
+
+        scanner.nextLine();
+
+        controller.addTask(title, description, deadline, priority);
+        System.out.println("Task added successfully!");
+    }
+
+    private void addReminder() {
+        System.out.print("Enter reminder title: ");
+        String title = scanner.nextLine();
+
+        System.out.print("Enter reminder description: ");
+        String description = scanner.nextLine();
+
+        System.out.print("Enter reminder deadline: ");
+        String deadline = scanner.nextLine();
+
+        System.out.print("Enter reminder time: ");
+        String time = scanner.nextLine();
+
+        controller.addReminder(title, description, deadline, time);
+        System.out.println("Reminder added successfully!");
+    }
+
+    private void showEvents() {
+        System.out.println("-------- List of Events --------");
+        controller.showEvents();
+    }
+
+    private void removeEvent() {
+        System.out.print("Enter the ID of the event to remove: ");
+        String eventId = scanner.nextLine();
+
+        if (controller.removeEvent(eventId)) {
+            System.out.println("Event removed successfully!");
+        } else {
+            System.out.println("No event found with ID " + eventId);
         }
     }
 }
